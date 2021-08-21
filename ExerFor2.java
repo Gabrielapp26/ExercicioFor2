4910aa7be8f80440fa1cffa1912bdfec122459c9
Gabriela Pellin Perosa <gpp260203@gmail.com>
2021-08-21 20:09:34 +0000

    Exercicio 2 de For

---
+++ ExerFor2.java
@@ -0,0 +1,10 @@
+//Imprima a soma de 1 até 1000
+public class ExerFor2{
+    public static void main(String[] args){
+        int soma = 0;
+        for(int x=1; x<=1000; x++){
+            soma += x;
+         System.out.println("A soma de todos os números é: "+soma);
+        }
+    }
+}
